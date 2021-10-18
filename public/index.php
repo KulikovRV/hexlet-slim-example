d3f9aa2d473ea2243d261443c467d6f7728cb210
Roman Kulikov <texasultra@mail.ru>
2021-10-18 11:41:54 +0300

    add new handlers

--- public/index.php
+++ public/index.php
@@ -9,9 +9,29 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    $response2 = $response->withStatus(204);
+    return $response2;
 });
+
+$app->get('/companies', function ($request, $response) {
+    return $response->write('GET /companies');
+});
+
+$app->post('/companies', function ($request, $response) {
+    return $response->write('POST /companies');
+});
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
 $app->run();
