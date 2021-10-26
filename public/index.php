13b8925c8b6ca7d5cad8b18b3ec23ba2d32200a3
Roman Kulikov <texasultra@mail.ru>
2021-10-26 11:45:18 +0300

    feat search form for users

--- public/index.php
+++ public/index.php
@@ -4,9 +4,53 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
+$courses = [
+    ['id'=> 1, 'name' => 'JS'],
+    ['id'=> 2, 'name' => 'PHP']
+];
+
+$users = [
+    'mike',
+    'mishel',
+    'adel',
+    'keks',
+    'kamila'
+];
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $filteredUsers = array_filter($users, fn($user) => str_contains($user, $term));
+    $params = ['users' => $filteredUsers];
+    return $this->get('renderer')->render($response, 'courses/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+
+/*
+$app->get('/courses', function ($request, $response) use ($courses) {
+    $params = [
+        'courses' => $courses
+    ];
+    return $this->get('renderer')->render($response, 'courses/index.phtml', $params);
+});
+*/
 
 $app->get('/', function ($request, $response) {
     $response2 = $response->withStatus(204);
@@ -19,10 +63,6 @@ $app->get('/companies', function ($request, $response) {
 
 $app->post('/companies', function ($request, $response) {
     return $response->write('POST /companies');
-});
-
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
 });
 
 $app->post('/users', function ($request, $response) {
